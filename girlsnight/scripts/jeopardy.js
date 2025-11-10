7967dea9d89f24d89ffdf36dcc18ab6bcf184750
Benceroni <bell.spencer@icloud.com>
2025-11-09 21:04:28 -0700

    yeah it kinda be working

--- girlsnight/scripts/jeopardy.js
+++ girlsnight/scripts/jeopardy.js
@@ -43,16 +43,157 @@ questions = [
         topic: "The Couple",
         questions : [
             {
-                question: "how old was bailey when?",
-                answer: "12",
-                points: 300,
+                question: "Where did Josiah and Bailey first meet? ",
+                answer: "",
+                points: 100,
                 show: true,
                 double: false
             },
             {
-                question : "how much did bailey",
-                answer : "144",
+                question : "What was their very first date?",
+                answer : "",
                 points : 200,
+                show: true,
+                double: false
+            },
+            {
+                question : "Where is the couple going on their honeymoon?",
+                answer : "",
+                points : 300,
+                show: true,
+                double: false
+            },
+            {
+                question : "How long has the couple been together for?",
+                answer : "",
+                points : 400,
+                show: true,
+                double: false
+            },
+            {
+                question : "How long was/has the couple been long-distance for?",
+                answer : "",
+                points : 500,
+                show: true,
+                double: false
+            }
+            ]
+    },
+    {
+        topic: "The Bride",
+        questions : [
+            {
+                question: "If Bailey could be any Disney Princess, which would it be?",
+                answer: "",
+                points: 100,
+                show: true,
+                double: false
+            },
+            {
+                question : "What is Bailey’s middle name?",
+                answer : "",
+                points : 200,
+                show: true,
+                double: false
+            },
+            {
+                question : "What was Bailey’s first impression of Josiah?",
+                answer : "",
+                points : 300,
+                show: true,
+                double: false
+            },
+            {
+                question : "If Bailey got arrested, what would it be for?",
+                answer : "",
+                points : 400,
+                show: true,
+                double: false
+            },
+            {
+                question : "What is Bailey’s favorite book/series?",
+                answer : "",
+                points : 500,
+                show: true,
+                double: false
+            }
+            ]
+    },
+    {
+        topic: "The Groom",
+        questions : [
+            {
+                question: "How did he propose?",
+                answer: "",
+                points: 100,
+                show: true,
+                double: false
+            },
+            {
+                question : "What is Josiah’s middle name?",
+                answer : "",
+                points : 200,
+                show: true,
+                double: false
+            },
+            {
+                question : "What are Josiah’s allergies?",
+                answer : "",
+                points : 300,
+                show: true,
+                double: false
+            },
+            {
+                question : "What did Josiah get his degree in at college?",
+                answer : "",
+                points : 400,
+                show: true,
+                double: false
+            },
+            {
+                question : "What was Josiah’s first impression of Bailey?",
+                answer : "",
+                points : 500,
+                show: true,
+                double: false
+            }
+            ]
+    },
+    {
+        topic: "Before Josiah",
+        questions : [
+            {
+                question: "How many guys has Bailey dated?",
+                answer: "",
+                points: 100,
+                show: true,
+                double: false
+            },
+            {
+                question : "How old was Bailey when she had her first kiss?",
+                answer : "",
+                points : 200,
+                show: true,
+                double: false
+            },
+            {
+                question : "What did Bailey’s dream wedding look like as a kid/high school?",
+                answer : "",
+                points : 300,
+                show: true,
+                double: false
+            },
+            {
+                question : "Who is Bailey’s celebrity crush/hall pass?",
+                answer : "",
+                points : 400,
+                show: true,
+                double: false
+            },
+            {
+                question : "What has been Bailey’s biggest relationship regret prior to Josiah?",
+                answer : "",
+                points : 500,
                 show: true,
                 double: false
             }
@@ -69,18 +210,30 @@ questions.forEach(i => {
 });
 
 function renderAllBlocks(){
-    questions.forEach(topic => {
-        
-        topic.questions.forEach(element => {
-            gameBoard.innerHTML += `<p>${element.points}</p>`
+    gameBoard.innerHTML = ''
+    questions.forEach(questionCluster => {
+        var builtBlock = `<div>`
+        questionCluster.questions.forEach(element => {
+            // builtBlock += `<p>${element.points}</p>`
+            if (element.show == true){
+                builtBlock += buildBlock(element.points,element.question,questions.indexOf(questionCluster),questionCluster.questions.indexOf(element));
+            }
+            else{
+                builtBlock += buildEmptyBlock();
+            }
+            // console.log(buildBlock(element.points,element.question))
         });
+        builtBlock += "</div>"
+        gameBoard.innerHTML += builtBlock
+        builtBlock=""
     });
+    // console.table(questions);
 }
 
 renderAllBlocks();
 
-function buildBlock(points){
-    return `<button type="button" class="block" onclick="showQuestion(${question},)">${points}</button>`
+function buildBlock(points,question,indexZero,indexOne){
+    return `<button type="button" class="block" onclick="showQuestion('${question}','${points}','${indexZero}','${indexOne}')">${points}</button>`
 }
 
 function buildEmptyBlock(){
@@ -91,10 +244,16 @@ function buildTopicBlock(topicName){
     return `<h3 class="topicHeading">${topicName}</h3>`
 }
 
-function showQuestion(question,points){
-
-
+function showQuestion(_question,points,index0,index1){
+    // console.log(`question: ${_question} points: ${points}`);
+    // console.log(index1)
+    // console.log(questions[index0].questions[index1]);
+    questions[index0].questions[index1]["show"] = false;
+    console.log(questions[index0].questions[index1]["show"])
+    renderAllBlocks();
 }
+
+
 
 function resetBoard(){
     document.querySelector("main").innerHTML =`<div id="boardTitles"></div><div id="board"></div>`
